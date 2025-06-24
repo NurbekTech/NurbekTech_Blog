8d847ea6bcc95a920207dc1be711e40eaec9c80f
nurbekomar <omarnurbek13@gmail.com>
2025-06-24 10:46:26 +0500

    add model and static

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 from blog import views
 
-urlpatterns = [path("", views.home)]
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("blog/", views.blog, name="blog"),
+    path("course/", views.course, name="course"),
+    path("contact/", views.contact, name="contact"),
+]
--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,28 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+
+menu = [
+    {"menu_name": "Блог", "menu_url": "blog"},
+    {"menu_name": "Курсы", "menu_url": "course"},
+    {"menu_name": "Контакты", "menu_url": "contact"},
+]
+
+
 def home(request):
-    return HttpResponse("<h1>Hello Django!</h1>")
+    data = {"menu": menu}
+    return render(request, "blog/home.html", context=data)
+
+
+def blog(request):
+    data = {"menu": menu}
+    return render(request, "blog/blog.html", context=data)
+
+
+def course(request):
+    data = {"menu": menu}
+    return render(request, "blog/course.html", context=data)
+
+
+def contact(request):
+    return render(request, "blog/contact.html", {"menu": menu})
